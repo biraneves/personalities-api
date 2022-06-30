3b0feb7f322efbe30ede47474a1850f77d66df96
biraneves <ubirajara.neves@usp.br>
2022-06-30 12:33:57 -0300

    Estrutura básica e retorno por json

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,12 +1,21 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/biraneves/personalities-api/models"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprint(w, "Home Page")
+
+}
+
+func AllPersonalities(w http.ResponseWriter, r *http.Request) {
+
+	json.NewEncoder(w).Encode(models.Personalities)
 
 }
--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/biraneves/personalities-api/models"
 	"github.com/biraneves/personalities-api/routes"
 )
 
 func main() {
+
+	models.Personalities = []models.Personality{
+		{Name: "Name 1", History: "History 1"},
+		{Name: "Name 2", History: "History 2"},
+	}
 
 	fmt.Println("Iniciando o servidor Rest com Go.")
 	routes.HandleRequest()
